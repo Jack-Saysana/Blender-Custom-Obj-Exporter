c786317802f5d6d4a6dce55eb5efc0480bb6bc24
Jack Saysana <saysanabjack@gmail.com>
2022-11-12 01:24:56 -0500

    Fixed whitespace bug

--- export.py
+++ export.py
@@ -31,7 +31,7 @@ def write_data(filepath):
     obj_file = open(filepath, 'w', encoding='utf-8')
     mtl_file = open(filepath[0:-4] + ".mtl", 'w', encoding='utf-8')
     
-    print("mtllib %s.mtl" % filepath[0:-4], file=obj_file)
+    print("mtllib %s.mtl" % bpy.path.basename(filepath[0:-4]), file=obj_file)
     for object in bpy.data.objects:
         if object.type == 'MESH':
             mesh_data = object.data
@@ -77,7 +77,10 @@ def write_data(filepath):
             for polygon in mesh_data.polygons:
                 print("f", end =" ", file=obj_file)
                 for index in range(polygon.loop_start, polygon.loop_start + polygon.loop_total):
-                    print("%d/%d/%d" % (mesh_data.loops[index].vertex_index + 1, get_index(uvs, mesh_data.uv_layers.active.data[index].uv, 2) + 1, get_index(normals, polygon.normal, 3) + 1), end =" ", file=obj_file)
+                    if index < polygon.loop_start + polygon.loop_total - 1:
+                        print("%d/%d/%d" % (mesh_data.loops[index].vertex_index + 1, get_index(uvs, mesh_data.uv_layers.active.data[index].uv, 2) + 1, get_index(normals, polygon.normal, 3) + 1), end =" ", file=obj_file)
+                    else:
+                        print("%d/%d/%d" % (mesh_data.loops[index].vertex_index + 1, get_index(uvs, mesh_data.uv_layers.active.data[index].uv, 2) + 1, get_index(normals, polygon.normal, 3) + 1), end ="", file=obj_file)
                 print("", file=obj_file)
     obj_file.close()
     mtl_file.close()
